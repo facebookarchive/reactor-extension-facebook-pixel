2a7ce4ed4b5a73394c17cc4a2b90d18631c18e25
Serban Stancu <stancu@adobe.com>
2017-09-29 10:06:45 -0600

    Remove advanced options section.

--- src/view/extensionConfiguration/__tests__/extensionConfiguration.test.jsx
+++ src/view/extensionConfiguration/__tests__/extensionConfiguration.test.jsx
@@ -43,8 +43,7 @@ describe('parameters', () => {
     pixelIdField.props.onChange('123456');
 
     expect(extensionBridge.getSettings()).toEqual({
-      pixelId: '123456',
-      sendDataToAdobe: undefined
+      pixelId: '123456'
     });
   });
 
--- src/view/extensionConfiguration/extensionConfiguration.jsx
+++ src/view/extensionConfiguration/extensionConfiguration.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Textfield from '@coralui/redux-form-react-coral/lib/Textfield';
 import { Field } from 'redux-form';
 import DecoratedInput from '@reactor/react-components/lib/reduxForm/decoratedInput';
-import AdvancedOptions, { formConfig as advancedOptionsFormConfig } from './components/advancedOptions';
-import { mergeConfigs } from '../utils/formConfigUtils';
 
 export default ({ ...props }) => (
   <div>
@@ -18,31 +16,27 @@ export default ({ ...props }) => (
         />
       </div>
     </label>
-    <AdvancedOptions fields={ props.fields } />
   </div>
 );
 
-export const formConfig = mergeConfigs(
-  advancedOptionsFormConfig,
-  {
-    settingsToFormValues: (values, settings) => ({
-      ...values,
-      pixelId: settings.pixelId
-    }),
-    formValuesToSettings: (settings, values) => ({
-      ...settings,
-      pixelId: values.pixelId
-    }),
-    validate(errors, values) {
-      errors = {
-        ...errors
-      };
+export const formConfig = {
+  settingsToFormValues: (values, settings) => ({
+    ...values,
+    pixelId: settings.pixelId
+  }),
+  formValuesToSettings: (settings, values) => ({
+    ...settings,
+    pixelId: values.pixelId
+  }),
+  validate(errors, values) {
+    errors = {
+      ...errors
+    };
 
-      if (!values.pixelId) {
-        errors.pixelId = 'Please specify your Facebook Pixel ID';
-      }
-
-      return errors;
+    if (!values.pixelId) {
+      errors.pixelId = 'Please specify your Facebook Pixel ID';
     }
+
+    return errors;
   }
-);
+};
